14cdb66adf757dda3d2b910247f3c32ef5336054
Ailin78 <47622586+Ailin78@users.noreply.github.com>
2019-02-14 07:42:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,16 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.login('NDgzMjgwNjQ4Mzc5ODI2MTc3.D0abng.cI-qYA7LucAlc_DoklvKIB-UYhU');
+// THIS  MUST  BE  THIS  WAY
+client.login(NDgzMjgwNjQ4Mzc5ODI2MTc3.D0abng.cI-qYA7LucAlc_DoklvKIB-UYhU);
